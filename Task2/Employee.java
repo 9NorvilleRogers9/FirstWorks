39f1b72db8854ec3d7688ea77bcbc11f871bdfb1
Sergeev Vladimir <vld.152.09@gmail.com>
2019-11-12 12:48:52 +0300

    Last update

--- Task2/Employee.java
+++ Task2/Employee.java
@@ -52,6 +52,26 @@ public class Employee {
     {
         return salary*=percent;
     }
+	@Override
+    public int hashCode() {
+        int result = 17;
+        result = 19 * result + firstNmae.hashCode();
+        result = 19 * result + id;
+        result = 19 * result + lastName.hashCode();
+        result = 19 * result + salary;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj  ==  null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Employee employee = (Employee) obj;
+        return employee.id == id && employee.firstNmae.equals(firstNmae) &&
+                employee.lastName.equals(lastName) && employee.salary == salary;
+    }
 
     @Override
     public String toString() {
